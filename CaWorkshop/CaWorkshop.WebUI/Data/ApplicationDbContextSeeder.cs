e7cccf4ae47b7f562902980aa97c76be5860c968
Sam Xian <xianyinyin05181986@gmail.com>
2020-07-09 11:03:47 +1000

    Building a Data Model with EF Core

---
+++ CaWorkshop/CaWorkshop.WebUI/Data/ApplicationDbContextSeeder.cs
@@ -0,0 +1,35 @@
+﻿using CaWorkshop.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.WebUI.Data
+{
+    public static class ApplicationDbContextSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            if (context.TodoLists.Any())
+            {
+                return;
+            }
+
+            var list = new TodoList
+            {
+                Title = "Death List Five",
+                Items =
+                {
+                    new TodoItem { Title = "O-Ren Ishii", Done = true },
+                    new TodoItem { Title = "Vernita Green", Done = true },
+                    new TodoItem { Title = "Budd", Done = true },
+                    new TodoItem { Title = "Ellie Driver" },
+                    new TodoItem { Title = "Bill" }
+                }
+            };
+
+            context.TodoLists.Add(list);
+            context.SaveChanges();
+        }
+    }
+}
