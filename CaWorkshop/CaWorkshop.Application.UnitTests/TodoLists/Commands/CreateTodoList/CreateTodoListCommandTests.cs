57502738260b24762f7f3371e8b609eebad20b7e
Sam Xian <xianyinyin05181986@gmail.com>
2020-07-10 12:13:15 +1000

    Unit Testing Application Logic

---
+++ CaWorkshop/CaWorkshop.Application.UnitTests/TodoLists/Commands/CreateTodoList/CreateTodoListCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
+using CaWorkshop.Infrastructure.Persistence;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaWorkshop.Application.UnitTests.TodoLists.Commands.CreateTodoList
+{
+
+    public class CreateTodoListCommandTests : TestFixture
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateTodoListCommandTests()
+        {
+            _context = Context;
+        }
+
+        // 🔥 Code omitted for clarity 🔥
+    }
+}
